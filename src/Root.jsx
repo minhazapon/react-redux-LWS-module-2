e54a769b921de0c076e7c6320df98a71b9051541
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-17 19:11:12 -0800

    added file

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Nav from "./Nav"
 
 
 function Root() {
     return (
         <div>
+
+            <Nav></Nav>
+            <Outlet></Outlet>
 
         </div>
     )
